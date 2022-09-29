c69d284cbb2a5dcf6f0e531a227945908f8ab058
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2022-09-29 19:45:09 -0300

    Node of Madness

--- aula 01/teste.js
+++ aula 01/teste.js
@@ -1,21 +1,21 @@
 //CommonJS - importar e exportar módulos
 const os = require('os')
 
+const totalMem = parseInt(os.totalmem() / 1024 / 1024);
+const freeMem = parseInt(os.freemem() / 1024 / 1024);
+const percent = parseFloat((freeMem/totalMem)*100).toFixed(2);
 
+setInterval(() => {
+    const memoria = {
+        total: `${totalMem} MB`,
+        free: `${freeMem} MB`,
+        percent:`${percent} %`
+    }
 
-//const totalMem = parseInt(os.totalmem() / 1024 / 1024)
-//const freeMem = parseInt(os.freemem() / 1024 / 1024)
-
-
-const memeoria = {
-    total: `${totalMem} MB`,
-    free: `${freeMem} MB`
-}
-
-console.clear()
-console.table(memoria)
+    console.clear()
+    console.table(memoria)
+})
 
 //console.log(`Memória Livre: ${freeMem}`, `Total de Memória: ${totalMem}`)
 
 //console.log(os.freemem() / 1024 / 1024, os.totalmem())
-
