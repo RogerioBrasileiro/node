66a698add4d9da24350f5daa52bcf1c9ff70c30a
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2022-10-04 20:54:16 -0300

    Server creation

--- aula 01/teste.js
+++ aula 01/teste.js
@@ -1,7 +1,7 @@
 //CommonJS - importar e exportar módulos
 const os = require('os');
 const { bytestoGB, bytesToMB } = require('../aula 02/convertStorageUnit');
-const fs = require (fs);
+const fs = require ('fs');
 
 const totalMem = parseFloat(
     bytestoGB(os.totalmem()).toFixed(2)
@@ -20,14 +20,16 @@ setInterval(() => {
         percent: `${percent} %`
     }
 
+    var dateTime = new Date().toLocaleString()
+    var print = `\n${dateTime}\n${JSON.stringify(memoria)}`
     console.clear();
     console.table(memoria);
-    fs.appendFile('../aula 02/log.txt',
-    'Inseriondo log:',
+    fs.appendFile('../aula 02/log.txt', print
+    ,
     'utf-8',
-    () => console.log('Log inserido.'));
+    () => console.log(JSON.stringify(memoria)));
 }, 1000)
 
 //console.log(`Memória Livre: ${freeMem}`, `Total de Memória: ${totalMem}`)
 
-//console.log(os.freemem() / 1024 / 1024, os.totalmem())
+//console.log(os.freemem() / 1024 / 1024, os.totalmem()) 0\
---
+++ aula 03/server.js
@@ -0,0 +1,5 @@
+const http = require ("http");
+
+http.createServer((req, res) => {
+    console.log (req, res);
+}).listen (5000, () => console.log('Servidor OK'));
